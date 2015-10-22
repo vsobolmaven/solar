fbce2123e15fa331d3749d231f2061108f1d5894
Andriy Mylenkyi <amylenkyi@maven.co>
2015-10-22 09:15:20 +0300

    Up version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def requires():
 
 setup(
     name="solar",
-    version="0.3.999",
+    version="0.4.001",
     author="Alexander Koval",
     author_email="kovalidis@gmail.com",
     description=("A library to use solr in python projects."),
